152010e72d02d314192294482f8b7add1640f9cb
Nikolai Nozhenko <nik.nozhenko@gmail.com>
2016-08-31 02:04:20 +0300

    use six in tests for Python 2 and 3 compatibility

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import pyptly
+import six
 
-# assert_is_instance appeared in python 3.2 and was backported to 2.7
 try:
     from nose.tools import assert_is_instance
 except ImportError:
@@ -12,4 +12,4 @@ def test_aptly_version():
     aptly = pyptly.Aptly("http://127.0.0.1:8080")
     version = aptly.aptly_version()
     assert_is_instance(version, dict)
-    assert_is_instance(version['Version'], unicode)
+    assert_is_instance(version['Version'], six.string_types)
