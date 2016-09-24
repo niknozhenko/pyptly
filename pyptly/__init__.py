66442770fc5ad85a611e83fb1395f10014276bd0
Nikolai Nozhenko <nik.nozhenko@gmail.com>
2016-09-25 02:07:33 +0300

    add missing an r prefix, import Aptly at the top of __init__.py

--- pyptly/__init__.py
+++ pyptly/__init__.py
@@ -1,6 +1,6 @@
 """
 Python wrapper for the Aptly API
 """
-__version__ = '1.0.1'
-
 from pyptly.api import Aptly
+
+__version__ = '1.0.1'
--- pyptly/utils.py
+++ pyptly/utils.py
@@ -5,11 +5,11 @@ def prefix_sanitized(prefix):
     https://www.aptly.info/doc/api/publish/
     """
     # replace '.' with ':.'
-    prefix = re.sub('^\.$', ':.', prefix)
+    prefix = re.sub(r'^\.$', ':.', prefix)
     # replace single underscores with double underscores
-    prefix = re.sub('(?<!_)_(?!_)', '__', prefix)
+    prefix = re.sub(r'(?<!_)_(?!_)', '__', prefix)
     # replace slashes with single underscores
-    prefix = re.sub('/', '_', prefix)
+    prefix = re.sub(r'/', '_', prefix)
     return prefix
 
 
